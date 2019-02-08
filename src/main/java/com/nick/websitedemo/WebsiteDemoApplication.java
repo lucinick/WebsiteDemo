9667036481a6b2dc825c50a70280ca320b93bd16
hungnd <hungnd.iam@gmail.com>
2019-02-08 20:09:19 +0700

    Init project

---
+++ src/main/java/com/nick/websitedemo/WebsiteDemoApplication.java
@@ -0,0 +1,14 @@
+package com.nick.websitedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteDemoApplication.class, args);
+	}
+
+}
+
